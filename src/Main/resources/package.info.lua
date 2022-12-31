5ccd8b1b3871de5a3b24a3eb7a19f0019df74efc
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-12-31 15:16:31 +0100

    finish

---
+++ src/Main/resources/package.info.lua
@@ -0,0 +1,36 @@
+-- See https://github.com/Dot-lua/TypeWriter/wiki/package.info.lua-format for more info
+
+return { InfoVersion = 1, -- Dont touch this
+
+    ID = "HttpClient", -- A unique id 
+    Name = "HttpClient",
+    Description = "A typewriter HttpClient",
+    Version = "1.0.0",
+
+    Author = {
+        Developers = {
+            "CoreByte"
+        },
+        Contributors = {}
+    },
+
+    Dependencies = {
+        Luvit = {
+            "creationix/coro-http",
+            "luvit/json"
+        },
+        Git = {},
+        Dua = {}
+    },
+
+    Contact = {
+        Website = "https://cubic-inc.ga",
+        Source = "https://github.com/Dot-lua/TypeWriter/",
+        Socials = {}
+    },
+
+    Entrypoints = {
+        Main = "me.corebyte.Http"
+    }
+
+}
